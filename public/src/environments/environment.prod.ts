6a57093103f22cdfeb7e29de67700dcfac62a376
Vitalii Lyskov <vet1kk@gmail.com>
2026-03-30 02:44:10 +0300

    feat: integrate ngx-google-analytics for enhanced tracking

---
+++ public/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+import { Env, EnvConfig } from '../app/models';
+
+export const environment: EnvConfig = {
+  production: true,
+  apiUrl: '/api',
+  env: Env.PROD,
+  gaMeasurementId: 'G-03B8C4EF26'
+};
