9930fd98b646401245cf422dec561201a3d32cb1
Vitalii Lyskov <vet1kk@gmail.com>
2026-03-30 00:36:25 +0300

    chore: setup azure deployment pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+set -e
+
+APP_ROOT="/home/site/wwwroot"
+
+chown -R www-data:www-data "$APP_ROOT/storage" "$APP_ROOT/bootstrap/cache"
+chmod -R 775 "$APP_ROOT/storage" "$APP_ROOT/bootstrap/cache"
+
+cd "$APP_ROOT"
+composer deploy
